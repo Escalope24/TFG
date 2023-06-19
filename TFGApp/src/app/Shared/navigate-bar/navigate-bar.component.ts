9ca5de8f4e8e8bccc610c7a30d4ed607d2d26af3
Escalope24 <noemoros24@gmail.com>
2023-06-19 18:01:21 +0200

    Ultimos retoques

--- TFGApp/src/app/Shared/navigate-bar/navigate-bar.component.ts
+++ TFGApp/src/app/Shared/navigate-bar/navigate-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CONSTANTS } from '../../Routes/routes';
+import { UserServiceService } from 'src/app/User/user-service.service';
 
 @Component({
   selector: 'app-navigate-bar',
@@ -8,7 +9,7 @@ import { CONSTANTS } from '../../Routes/routes';
   styleUrls: ['./navigate-bar.component.scss']
 })
 export class NavigateBarComponent {
-  constructor(private _router:Router){}
+  constructor(private _router:Router, private _userService:UserServiceService){}
 
   goToObjectives(){
     this._router.navigate([CONSTANTS.ROUTES.OBJECTIVES.OBJECTIVE])
@@ -18,5 +19,14 @@ export class NavigateBarComponent {
   }
   goToSocial(){
     this._router.navigate([CONSTANTS.ROUTES.EVENTS.EVENTS_MENU])
+  }
+
+  logOut(){
+    this._userService.logOut().then(()=>{
+      this._goToInit()
+    });
+  }
+  private _goToInit(){
+    this._router.navigate([CONSTANTS.ROUTES.USER.USERMENU])
   }
 }
